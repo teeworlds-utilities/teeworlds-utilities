f7d5d52d0ee1f1946ec0460aeb2b7136fa31b9b7
theobori <theo1.bori@epitech.eu>
2021-12-07 21:59:13 +0100

    Added gameskin emoticons to the data

--- tests/test.js
+++ tests/test.js
@@ -1,25 +1,23 @@
 const image = require("../src/lib");
 
-const SRC = "./data/greyfox.png"
+const SRC_SKIN = "./data/greyfox.png"
+const SRC_GAME = "./data/game.png"
+const SRC_EMOTICONS = "./data/emoticons.png"
 
-const extractSkin = async () => {
-    const asset = new image.TwAsset(SRC, "skin")
+const extractTest = async () => {
+    const asset = new image.TwAsset(SRC_EMOTICONS, "emoticons")
 
     try {
         await asset.preprocess()
-        //asset.extractAll()
-        asset.extract("foot")
-        asset.extract("foot")
-        asset.extract("hand")
-        asset.save("./tmp")
+        asset.extractAll()
+        //asset.extract("foot")
+        //asset.extract("foot")
+        //asset.extract("hand")
+        asset.save("./emoticons")
     } catch (err) {
         console.log(err)
     }
 
 }
 
-const extractGameskin = async () => {
-    ;
-} 
-
-extractSkin()
+extractTest()
