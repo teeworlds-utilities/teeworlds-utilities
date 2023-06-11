62d9a6617353f4fedb5b8c44703259cf5c656d38
Théo Bori <theo1.bori@epitech.eu>
2023-06-11 23:52:51 +0200

    update: export needed feature into from `index.ts`

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,34 @@
-// export {TwAssetExtractor} from './asset';
-// export {TwAssetChanger} from './changer';
-// export {TwAssetFix} from './fix';
-// export {TwSceneMaker} from './scene';
-// export {TwPersonalCard} from './card';
+import Skin from "./asset/skin";
+import Gameskin from "./asset/gameskin";
+import Particule from "./asset/particule";
+import Emoticon from "./asset/emoticon";
+
+import Scene from './scene';
+
+export { Skin, Gameskin, Particule, Emoticon };
+export { Scene };
+export { fixAssetSize } from './fix';
+export { PersonalCard } from "./card";
+
+export {
+  AssetPart,
+  AssetKind,
+  SkinPart,
+  EyeSkinPart,
+  GameskinPart,
+  ParticulePart,
+  EmoticonPart,
+  AssetHelpSize
+} from './asset/part';
+
+export {
+  IAsset
+} from './asset/base';
+
+export {
+  IColor,
+  ColorCode,
+  ColorHSL,
+  ColorRGB,
+  ColorRGBA
+} from './color';
